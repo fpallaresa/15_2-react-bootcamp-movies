17459e4563b858905cebc46c5f24c1cbd21db1ec
fpallares <fpallares@prensaiberica.es>
2024-02-27 13:40:05 +0100

    add eslint file with react version deteced

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,31 @@
 module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended'
-  ],
+  extends: ['standard', 'plugin:react/recommended'],
   overrides: [
     {
       env: {
         node: true,
-        jest: true
+        jest: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-        '**/*.test.js',
-        '**/*.spec.js'
-      ],
+      files: ['.eslintrc.{js,cjs}', '**/*.test.js', '**/*.spec.js'],
       parserOptions: {
         sourceType: 'module',
-      }
-    }
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ['react'],
   rules: {
     semi: 'off',
     'react/react-in-jsx-scope': 'off',
@@ -39,6 +35,6 @@ module.exports = {
     'jsx-quotes': [2, 'prefer-single'],
     'multiline-ternary': 'off',
     'react/no-unescaped-entities': 'off',
-    'comma-dangle': 'off'
-  }
-}
+    'comma-dangle': 'off',
+  },
+};
